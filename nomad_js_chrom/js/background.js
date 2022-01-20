8c1b4883e471b07f7092ce7a46a1fc5f359fe04c
chooing <chumil7432@gmail.com>
2022-01-20 23:20:40 +0900

    js_6.0~6.1_end

---
+++ nomad_js_chrom/js/background.js
@@ -0,0 +1,5 @@
+const images = ["0.jpg", "1.jpg", "2.jpg"];
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgImg = document.createElement("img");
+bgImg.src = `img/${chosenImage}`;
+document.body.append(bgImg);
