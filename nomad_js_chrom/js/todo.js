36673424834ba0c3a12da8645a60f40746ef7148
chooing <chumil7432@gmail.com>
2022-01-21 23:07:14 +0900

    js 7.2_end

---
+++ nomad_js_chrom/js/todo.js
@@ -0,0 +1,30 @@
+const todoForm = document.getElementById("todoForm");
+const todoInput = todoForm.querySelector("input");
+const todoList = document.getElementById("todoList");
+
+function deleteToDo(event) {
+  const deleteLi = event.target.parentElement;
+  deleteLi.remove();
+}
+
+function paintToDo(newTodo) {
+  const ElLi = document.createElement("li");
+
+  const ElSpan = document.createElement("span");
+  ElSpan.innerText = newTodo;
+
+  const ElButton = document.createElement("button");
+  ElButton.innerText = "❌";
+  ElButton.addEventListener("click", deleteToDo);
+
+  ElLi.append(ElSpan, ElButton);
+  todoList.append(ElLi);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = todoInput.value;
+  todoInput.value = "";
+  paintToDo(newTodo);
+}
+todoForm.addEventListener("submit", handleToDoSubmit);
