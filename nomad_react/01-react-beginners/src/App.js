58284bab3ce101cfe98685395daea9b5fbc8900d
chooing <chumil7432@gmail.com>
2022-01-29 22:38:05 +0900

    react 6.0 end

--- nomad_react/01-react-beginners/src/App.js
+++ nomad_react/01-react-beginners/src/App.js
@@ -1,11 +1,18 @@
 import Button from "./Button";
 import styled from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+  console.log("render");
   return (
     <div>
       <h1 className={styled.title}>Welcome Back!</h1>
-      <Button text={"continues"} />
+      <button onClick={onClick}>Click me</button>
+      <div>{counter}</div>
     </div>
   );
 }
